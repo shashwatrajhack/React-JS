fba09d5a55221cf133fdd0a127290cdb2e4073eb
shashwatrajhack <er.shashwatraj@gmail.com>
2025-01-04 18:02:01 +0530

    counter in react

--- counter-app/src/App.jsx
+++ counter-app/src/App.jsx
@@ -1,37 +1,23 @@
-import { useState } from 'react'
-
-
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  
+  const [count, setCount] = useState(0);
 
   return (
     <div>
-
-      <CustomButton count = {count} setCount = {setCount}></CustomButton>
-   
+      <CustomButton count={count} setCount={setCount}></CustomButton>
     </div>
-  )
+  );
 }
-
 
 //COMPONENT
 
-
-function CustomButton(props){
-  function onClickHandler(){
+function CustomButton(props) {
+  function onClickHandler() {
     props.setCount(props.count + 1);
   }
 
-  return(
-    <button onClick={onClickHandler}>
-      Counter {props.count}
-    </button>
-  )
-
-
+  return <button onClick={onClickHandler}>Counter {props.count}</button>;
 }
 
-export default App
+export default App;
