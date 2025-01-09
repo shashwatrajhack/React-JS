5c56b248075722303a6fe2ad8f76f781bfdc7521
shashwatrajhack <er.shashwatraj@gmail.com>
2025-01-10 02:59:59 +0530

    re-rendering in app

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,9 +1,17 @@
-import React, {Fragment} from "react";
+import React, {Fragment, useState} from "react";
+//import { useState } from "react";
+//destructuring of fragment
 
 function App() {
+  const[title,setTitle] = useState("world is here");
+
+  function updateTitle(){
+    setTitle("world is " + Math.random());
+  }
   return (
     <Fragment>
-      <Header title="world is here" />
+      <button onClick={updateTitle}>Update the Title1</button>
+      <Header title= {title} />
       <Header title="this world is beautiful" />
     </Fragment>
   );
