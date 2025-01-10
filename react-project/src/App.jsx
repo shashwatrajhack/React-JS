503ea8fceca7c2650de24cf3e01847f3f62f5d48
shashwatrajhack <er.shashwatraj@gmail.com>
2025-01-10 12:49:06 +0530

    memoizaton in react

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,23 +1,28 @@
-import React, {Fragment, useState} from "react";
+import React, { Fragment, useState } from "react";
+//import PropTypes from 'prop-types';
 //import { useState } from "react";
 //destructuring of fragment
 
 function App() {
-  const[title,setTitle] = useState("world is here");
+  const [title, setTitle] = useState("world is here");
 
-  function updateTitle(){
+  function updateTitle() {
     setTitle("world is " + Math.random());
   }
   return (
     <Fragment>
       <button onClick={updateTitle}>Update the Title1</button>
-      <Header title= {title} />
+      <Header title={title} />
+      <Header title="this world is beautiful" />
+      <Header title="this world is beautiful" />
+      <Header title="this world is beautiful" />
       <Header title="this world is beautiful" />
     </Fragment>
   );
 }
-function Header({ title }) {
+
+const Header = React.memo(function Header({title}) {
   return <div>{title}</div>;
-}
+});
 
 export default App;
