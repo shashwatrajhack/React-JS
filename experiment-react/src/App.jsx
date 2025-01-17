c486c06f7ff60fba95b58b6c24578a9eddaeea5b
shashwatrajhack <er.shashwatraj@gmail.com>
2025-01-17 23:54:45 +0530

    react hooks useEffect

--- experiment-react/src/App.jsx
+++ experiment-react/src/App.jsx
@@ -5,18 +5,30 @@ import './App.css'
 import { useEffect } from 'react';
 
 function App() {
-  const [todo,setTodo] = useState([]);
+  const [todos,setTodos] = useState([]);
 
   useEffect(() =>{
     axios.get("https://sum-server.100xdevs.com/todos")
+    .then(function(response){
+      setTodos(response.data.todos);
+    })
 
   },[]);
 
   return (
     <>
-      
+      {todos.map(todo => <Todo title={todo.title} description = {todo.description} />)}
     </>
   )
+}
+
+function Todo({title,description}){
+  return <div>
+    <h1>{title}</h1>
+    {description}
+  </div>
+
+
 }
 
 export default App
