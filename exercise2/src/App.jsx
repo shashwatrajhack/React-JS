759ff51ef912d447490759741ca8173c1090f6a4
shashwatrajhack <er.shashwatraj@gmail.com>
2025-01-24 22:00:45 +0530

    useMemo hook

--- exercise2/src/App.jsx
+++ exercise2/src/App.jsx
@@ -1,21 +1,38 @@
 import { useState } from 'react'
 
 import './App.css'
+import { useMemo } from 'react';
 
 function App() {
   const [counter,setCounter] = useState(0);
   const [inputValue,setInputValue] = useState(1);
 
-
-  let count = 0;
-  for(let i = 0;i<=inputValue;i++){
-    count = count + i;
-  };
+  let count = useMemo(() => {
+    console.log("memo got called");
+    let finalCount = 0;
+    for(let i = 0;i<=inputValue;i++){
+    finalCount = finalCount + i;
+  }
+  return finalCount;
+},[inputValue]);
+  
 
 
   return (
     <div>
-      
+      <input onChange={function(e){
+        setInputValue(e.target.value)
+      }} placeholder='Sum of numbers from 1 to n'></input>
+      <br />
+      sum from 1 to {inputValue} is {count}
+      <br />
+      <button onClick={() => {
+        setCounter(counter+1);
+      }}>
+        Counter ({counter}) 
+
+      </button>
+
     </div>
   )
 }
