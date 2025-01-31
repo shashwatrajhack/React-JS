1e5ba6aaad5f03290f740514ca6e5925e126dde9
shashwatrajhack <er.shashwatraj@gmail.com>
2025-01-31 23:55:49 +0530

    routing

--- routingInReact/src/App.jsx
+++ routingInReact/src/App.jsx
@@ -2,14 +2,20 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter, Route } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      
-    </>
+    <div>
+      <div style={{background:"black", color:"white"}}>
+        hi this is the topbar
+      </div>
+      <BrowserRouter><Routes>
+        <Route></Route>
+        </Routes></BrowserRouter>
+    </div>
   )
 }
 
