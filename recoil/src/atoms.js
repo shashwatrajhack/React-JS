074465d9ea809b1febe5b41219e8e44ae5f03124
shashwatrajhack <er.shashwatraj@gmail.com>
2025-02-08 20:14:19 +0530

    recoil in react

---
+++ recoil/src/atoms.js
@@ -0,0 +1,18 @@
+
+
+export const networkAtom = atom({
+  key: "networkAtom",
+  default: 102,
+});
+export const jobAtom = atom({
+  key: "jobAtom",
+  default: 0
+});
+export const notificationAtom = atom({
+  key: "notificationAtom",
+  default: 12,
+});
+export const messagingAtom = atom({
+  key: "networkAtom",
+  default: 102,
+});
