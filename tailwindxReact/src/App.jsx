226067131d524d05d82b2949738caaa2eb72fd52
shashwatrajhack <er.shashwatraj@gmail.com>
2025-02-18 02:03:41 +0530

    tailwind colour components

--- tailwindxReact/src/App.jsx
+++ tailwindxReact/src/App.jsx
@@ -1,4 +1,4 @@
-
+import React from 'react'
 import './App.css'
 
 function App() {
@@ -6,12 +6,10 @@ function App() {
 
   return (
     <>
-     <div className='flex'>
-      <div style={{backgroundColor:"red"}}>hii</div>
-      <div style={{backgroundColor:"green"}}>hii</div>
-      <div style={{backgroundColor:"blue"}}>hii</div>
-      <div style={{backgroundColor:"yellow"}}>hii</div>
-
+     <div className='grid grid-cols-1 md:grid-cols-3'>
+      <div className='bg-green-500'>hiiio</div>
+      <div className='bg-yellow-500'>hiioioioi</div>
+      <div className='bg-blue-500'>hioii</div>
      </div>
     </>
   )
