1c9415639b56d8296549bd98fde8d4ec4c440559
shashwatrajhack <er.shashwatraj@gmail.com>
2025-02-17 23:29:59 +0530

    tailwind x react

---
+++ tailwindxReact/src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     <div className='flex'>
+      <div style={{backgroundColor:"red"}}>hii</div>
+      <div style={{backgroundColor:"green"}}>hii</div>
+      <div style={{backgroundColor:"blue"}}>hii</div>
+      <div style={{backgroundColor:"yellow"}}>hii</div>
+
+     </div>
+    </>
+  )
+}
+
+export default App
