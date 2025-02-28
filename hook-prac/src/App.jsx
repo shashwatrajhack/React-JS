ddaff5209557e5fd8eeb52c0899641f5e3eddbfb
shashwatrajhack <er.shashwatraj@gmail.com>
2025-02-28 12:50:02 +0530

    custom hooks

--- hook-prac/src/App.jsx
+++ hook-prac/src/App.jsx
@@ -1,28 +1,30 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
-import './App.css'
+const useMousePointer = () => {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
 
-function App() {
-  return (
-    <>
-      <MyComponent />
-    </>
-  )
-}
-
-function MyComponent() {
-  const [count, setCount] = useState(0);
-
-  const incrementCount = () => {
-    setCount(count + 1);
+  const handleMouseMove = (e) => {
+    setPosition({ x: e.clientX, y: e.clientY });
   };
 
+  useEffect(() => {
+    window.addEventListener('mousemove', handleMouseMove);
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, []);
+
+  return position;
+};
+
+function App() {
+  const mousePointer = useMousePointer();
+
   return (
-    <div>
-      <p>{count}</p>
-      <button onClick={incrementCount}>Increment</button>
-    </div>
-  );
+    <>
+      Your mouse position is {mousePointer.x} {mousePointer.y}
+    </>
+  )
 }
 
 export default App
