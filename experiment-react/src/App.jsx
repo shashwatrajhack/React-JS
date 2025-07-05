d192b012f1ff0dcfb35cfe5fa257b4396b341875
shashwatrajhack <er.shashwatraj@gmail.com>
2025-07-05 10:53:55 +0530

    keys in react

--- experiment-react/src/App.jsx
+++ experiment-react/src/App.jsx
@@ -1,34 +1,47 @@
-import React from 'react';
+import { useState } from "react";
 
+let counter = 4;
 
 
 function App() {
-  const [title, setTitle] = React.useState("Hello from my vlog");
+  const [todos, setTodos] = useState([
+    { id: 1, title: "Todo 1", description: "This is the first todo" },
+    { id: 2, title: "Todo 2", description: "This is second todo" },
 
-  function updateTitile(){
-    setTitle("Hello from my vlog " + Math.random());
+    { id: 3, title: "TOdo 3", description: "This is the third todo" },
+  ]);
+
+  function addTodo(){
+    setTodos([...todos,{id:counter++ , title: Math.random(), description: Math.random()}]);
   }
 
+  // const [title, setTitle] = React.useState("Hello from my vlog");
+
+  // function updateTitile(){
+  //   setTitle("Hello from my vlog " + Math.random());
+  // }
 
   return <div>
-
-    <button onClick={updateTitile}>Update the title</button>
-    <Header title={title}/>
-    <Header title="Hello from my vlog 2"/>
-    <Header title="Hello from my vlog 2"/>
-    <Header title="Hello from my vlog 2"/>
-    <Header title="Hello from my vlog 2"/>
-  </div>
-
-
+    <button onClick={addTodo}> Add a todo</button>
+    {todos.map(todo => <Todo key={todo.id} title = {todo.title} description = {todo.description} />)}
+  </div>;
 }
 
 
 
-const Header = React.memo(function Header({title}){
-  return<div>
-    {title}
-  </div>
-})
+function Todo({ title, description }) {
+  return (
+    <div>
+      <h1>{title}</h1>
+      <p>{description}</p>
+    </div>
+  );
+}
 
-export default App
+// const Header = React.memo(function Header({title}){
+//   return<div>
+//     {title}
+//   </div>
+// })
+
+export default App;
